c6715d455d72529647a984df5d0ba28cbb8d262b
chachodoo <chaiittocholula@gmail.com>
2026-09-23 13:03:38 -0400

    Reduce timeout for startLivingMosaic function

--- script.js
+++ script.js
@@ -508,7 +508,7 @@ if (mobileTiendaCaret) mobileTiendaCaret.style.transform = 'rotate(0deg)';
         } else if (pageName === 'bazar') {
             loadBazarGrid();
         } else if (pageName === 'experiencia') {
-            setTimeout(startLivingMosaic, 150);
+            setTimeout(startLivingMosaic, 100);
         } else if (pageName === 'menu') {
   menuNavigationState.currentPage = targetPage;
   updateMenuDisplay();
